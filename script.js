2a1d3d202a6056076e0d1d73923c4532a24cfbbb
Alex Hamon <alex.hmn18@gmail.com>
2024-05-31 16:57:22 +0200

    commit

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ let data = response.data;
 
 data.forEach(agents => {
 
-    if(agents.isPlayableCharacter===true){
+    if(agents.isPlayableCharacter === true){
         
-        
+        let rowElement = document.querySelector("#rowElement");
+
+        let colElement = document.createElement('div');
+        colElement.classList.add('col-lg-3', 'col-md-6', 'colAgents', 'mb-5');
+        rowElement.appendChild(colElement);
+
+        let linkElement = document.createElement('a');
+        linkElement.setAttribute('href', 'test');
+        linkElement.className = ("linkAgents");
+        linkElement.style.backgroundImage = `url(${agents.displayIcon})`;
+        colElement.appendChild(linkElement);
+
+        let titleElement = document.createElement('h2');
+        titleElement.textContent = (agents.displayName);
+        titleElement.className = ("titleAgents")
+        linkElement.appendChild(titleElement);
 
     }
 
