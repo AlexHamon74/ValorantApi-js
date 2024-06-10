b3eda27e6f97cf25b391bd271d9ef3e29986a3af
Alex Hamon <alex.hmn18@gmail.com>
2024-06-10 16:47:05 +0200

    update front + pages armes

--- listeArmes/armes.js
+++ listeArmes/armes.js
@@ -10,25 +10,35 @@ data.forEach(armes => {
     
     let rowElement = document.querySelector('#rowElement');
 
+    //COL
     let colElement = document.createElement('div');
-    colElement.classList.add('col-xl-6', 'colElementArmes');
+    colElement.classList.add('col-lg-4');
     rowElement.appendChild(colElement);
 
-    let borderElement = document.createElement('div');
-    borderElement.className = ('borderElementArmes');
-    colElement.appendChild(borderElement);
+    //Weapons CARD
+    let weaponsCard = document.createElement('div');
+    weaponsCard.className = ('weaponsCard');
+    colElement.appendChild(weaponsCard);
 
-    let titleElement = document.createElement('h2');
-    titleElement.textContent = (armes.displayName);
-    titleElement.className = ('h2-link')
-    borderElement.appendChild(titleElement);
+    //Weapons Icon
+    let weaponsIcon = document.createElement('div');
+    weaponsIcon.className = ('weaponsIcon');
+    weaponsCard.appendChild(weaponsIcon);
 
-    let imgElement = document.createElement('img');
-    imgElement.setAttribute('src', armes.displayIcon);
-    borderElement.appendChild(imgElement);
+    let weaponsImg = document.createElement('img');
+    weaponsImg.setAttribute('src', armes.displayIcon);
+    weaponsImg.className = ('weaponsImg');
+    weaponsIcon.appendChild(weaponsImg);
 
-    let categoryElement = document.createElement('h3');
-    categoryElement.textContent = ('TYPES // ' + armes.shopData.category);
-    borderElement.appendChild(categoryElement);
+    let weaponsName = document.createElement('h2');
+    weaponsName.textContent = (armes.displayName + '.');
+    weaponsName.className = ('weaponsName');
+    weaponsCard.appendChild(weaponsName);
+
+
+    let weaponsCategory = document.createElement('h3');
+    weaponsCategory.className = ('weaponsCategory');
+    weaponsCategory.textContent = ('TYPES // ' + armes.shopData.categoryText);
+    weaponsCard.appendChild(weaponsCategory);
 
 });
